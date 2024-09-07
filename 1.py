6b419c3dd78085d412dc36cd5b4febd9c3a55acd
Sungryeong Park <phantomviolet61@naver.com>
2024-09-07 18:24:09 +0900

    feat. matplotlib sin & cos

--- 1.py
+++ 1.py
@@ -3,8 +3,14 @@ import matplotlib.pyplot as plt
 
 #データ準備
 x = np.arange(0, 6, 0.1)#ゼロから6まで0.1間隔で作成
-y = np.sin(x)
+y1 = np.sin(x)
+y2 = np.cos(x)
 
 #グラフ描画
-plt.plot(x, y)
+plt.plot(x, y1, label="sin")
+plt.plot(x, y2, linestyle="--", label="cos") #cos関数は点線で描画
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("sin & cos")
+plt.legend()
 plt.show()
