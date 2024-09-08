0ec26f494d644148b468858e28b16fe6dae541a6
Sungryeong Park <phantomviolet61@naver.com>
2024-09-08 16:54:39 +0900

    feat. perceptron matrix and nand or

--- perceptron.py
+++ perceptron.py
@@ -1,8 +1,31 @@
-w1, w2, theta = 0.5, 0.5, 1.0
+import numpy as np
 
 def and_gate(x1, x2):
-    tmp = x1*w1 + x2*w2
-    if tmp <= theta:
+    x = np.array(x1, x2)
+    w = np.array([0.5 ,0.5])
+    b = -0.7
+    tmp = np.sum(x*w) + b
+    if tmp <= 0:
         return 0
-    elif tmp > theta:
+    else:
+        return 1
+
+def not_and_gate(x1, x2):
+    x = np.array(x1, x2)
+    w = np.array([-0.5 ,-0.5])
+    b = 0.7
+    tmp = np.sum(x*w) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+    
+def or_gate(x1, x2):
+    x = np.array(x1, x2)
+    w = np.array([0.5 ,0.5])
+    b = -0.2
+    tmp = np.sum(x*w) + b
+    if tmp <= 0:
+        return 0
+    else:
         return 1
