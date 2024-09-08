568be42a8729db3dc79ff295ab74c35928eba50b
Sungryeong Park <phantomviolet61@naver.com>
2024-09-08 17:18:00 +0900

    feat. xor gate

--- perceptron.py
+++ perceptron.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+#and¡¢or¡¢nand«²?«ÈªÏ«Ç?«¿ªÎ?ö·ªª?ª¨ªÆÎıúŞªÇª­ªë
 def and_gate(x1, x2):
     x = np.array(x1, x2)
     w = np.array([0.5 ,0.5])
@@ -24,6 +25,17 @@ def or_gate(x1, x2):
     x = np.array(x1, x2)
     w = np.array([0.5 ,0.5])
     b = -0.2
+    tmp = np.sum(x*w) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+#ª·ª«ª·xor«²?«ÈªÏperceptronªòŞÅªÃªÆÎıúŞª¹ªëªÎªÏÜôÊ¦ÒöªÀ¡£
+def xor_gate(x1, x2): 
+    x = np.array(x1, x2)
+    w = np.array([0.7 ,-0.4])
+    b = -0.6
     tmp = np.sum(x*w) + b
     if tmp <= 0:
         return 0
